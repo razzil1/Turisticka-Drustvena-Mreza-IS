6fc5abafd631e9ad0536fe7b25219f3a547705f0
mare95 <petricicmarko1995@gmail.com>
2018-12-28 21:00:20 +0100

    text

--- src/views/view/index.js
+++ src/views/view/index.js
@@ -34,23 +34,23 @@ class View extends Component {
                         </div>
                         <div className="view-info-wrapper">
                         <div className='view-single-info'>
-                            <span>Opis</span> <br /><span>Grad muzike, valcera, umetnosti, prelepih parkova, ukusne saher torte. Grad u kome ćete moći da kupujete do mile volje u dugim shopping ulicama. Veličanstven grad, s bogatim arithektonskim i kulturnim nasleđem, po svom šarmu i raskoši, blizak Parizu i Rimu.</span>
+                            <span>Opis</span> <br /><span>Moon&Stars hostel se nalazi u centru Barselone, u Gotskoj četvrti na 2 minuta (100m) od glavnog šetališta – La Ramble i 2km od plaže Barseloneta. Sobe su 1/2, 1/6 1/8 i svaka ima svoje kupatilo (u višekrevetnim sobama krevetu su na sprat). U hostelu se rentiraju posteljina (2,2eur), peškiri (2,2eur) i ćebad (3,3eur), a u dvokrevetnim sobama su uključeni u cenu. Kako je hostel smešten u najužem centru grada, autobus se ne može parkirati blizu I potrebno je ići pešice oko 20ak minuta. Usluga je na bazi noćenja sa doručkom.</span>
                         </div>
                         <div className="view-info-stuff">
                             <div>
-                                <div>Kreveti: 3</div>
+                                <div>Kreveti: 2</div>
                                 <div>Tus kabina: Da</div>
                                 <div>Wifi: Da</div>
                             </div>
                             <div>
-                                <div>Klima: Ne</div>
-                                <div>Terasa: Ne</div>
-                                <div>Pet friendly: Da</div>
+                                <div>Klima: Da</div>
+                                <div>Terasa: Da</div>
+                                <div>Pet friendly: Ne</div>
                             </div>
                             <div>
-                                <div>Kupatila: 2</div>
-                                <div>Sobe: 2</div>
-                                <div>Gosti: 4</div>
+                                <div>Kupatila: 1</div>
+                                <div>Sobe: 1</div>
+                                <div>Gosti: 2</div>
                             </div>
                         </div>
                         </div>
